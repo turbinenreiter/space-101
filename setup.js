8d93f5215e8a4a6e31456203b32e7dd878bbecc4
Sebastian Plamauer <oeplse@gmail.com>
2017-10-23 21:57:19 +0200

    Kinda working

--- setup.js
+++ setup.js
@@ -11,67 +11,118 @@ var Engine = Matter.Engine,
     Vector = Matter.Vector,
     Events = Matter.Events;
 
-// create world, engine and renderer
-var center = {x: window.innerWidth / 2, y: window.innerHeight / 2}
-var canvas = document.createElement('canvas');
-var context = canvas.getContext('2d');
-canvas.width = window.clientWidth;
-canvas.height = window.clientHeight;
+var center;
+var canvas;
+var context;
+var world;
+var engine;
+var render;
+var size;
+var force;
+var earth;
+var sc;
+var e1;
+var fligh_path = true;
+var center_path = false;
+var burning = false;
+var sc_positions;
+var sc_positions_burn;
 
-var world = World.create({gravity: {x: 0, y: 0, scale: 0}});
-var engine = Engine.create({world: world, timing: {timeScale: 1}});
-var render = Render.create({
-    engine: engine,
-    element: document.getElementsByClassName("space")[0],
-    canvas: canvas,
-    options: {width: window.innerWidth,
-              height: window.innerHeight,
-//              pixelRatio: 1,
-              background: '#212121',
-//              hasBounds: true,
-//              enabled: true,
-              wireframes: false,
-//              showSleeping: false,
-//              showDebug: true,
-//              showBroadphase: false,
-//              showBounds: false,
-//              showVelocity: true,
-//              showCollisions: true,
-//              showSeparations: false,
-//              showAxes: false,
-//              showPositions: false,
-//              showAngleIndicator: true,
-//              showIds: false,
-//              showShadows: false,
-//              showVertexNumbers: false,
-//              showConvexHulls: false,
-//              showInternalEdges: false,
-//              showMousePosition: false
-              }});
+function create_space(section_id) {
+    try {
+        document.getElementById(section_id).innerHTML = "";
+    } catch(error) {}
+    // create world, engine and renderer
+    center = {x: window.innerWidth / 2, y: window.innerHeight / 2}
+    canvas = document.createElement('canvas');
+    context = canvas.getContext('2d');
+    canvas.width = window.clientWidth;
+    canvas.height = window.clientHeight;
 
-Render.run(render);
+    world = World.create({gravity: {x: 0, y: 0, scale: 0}});
+    engine = Engine.create({world: world, timing: {timeScale: 1}});
+    render = Render.create({
+        engine: engine,
+        element: document.getElementById(section_id),
+        canvas: canvas,
+        options: {width: window.innerWidth,
+                  height: window.innerHeight,
+                  background: '#212121',
+                  wireframes: false,
+                  }});
 
-MatterAttractors.Attractors.gravityConstant = 0.01;
+    Render.run(render);
 
-// create bodies
-var size = 10;
-var force = size/(18000+size*10);
-var earth = Bodies.circle(center.x, center.y, 5*size,
-                          {frictionAir: 0,
-                           frictionStatic: 1,
-                           friction: 1,
-                           density: 0.01,
-                           restitution: 0,
-                           plugin: {attractors: [MatterAttractors.Attractors.gravity]},
-                           render: {fillStyle: '#2196F3',
-                                    strokeStyle: '#4CAF50',
-                                    lineWidth: 10}});
-var sc = Bodies.trapezoid(center.x, center.y-earth.circleRadius-size, 2*size, 3*size, 1,
-                          {frictionAir: 0,
-                           render: {fillStyle: '#3F51B5',
-                                    lineWidth: 0}});
+    MatterAttractors.Attractors.gravityConstant = 0.01;
 
-var e1 = Vector.sub(sc.position, earth.position);
+    // create bodies
+    size = 10;
+    force = size/(18000+size*10);
+    earth = Bodies.circle(center.x, center.y, 5*size,
+                              {frictionAir: 0,
+                               frictionStatic: 1,
+                               friction: 1,
+                               density: 0.01,
+                               restitution: 0,
+                               plugin: {attractors: [MatterAttractors.Attractors.gravity]},
+                               render: {fillStyle: '#2196F3',
+                                        strokeStyle: '#4CAF50',
+                                        lineWidth: 10}});
+    sc = Bodies.trapezoid(center.x, center.y-earth.circleRadius-size, 2*size, 3*size, 1,
+                              {frictionAir: 0,
+                               render: {fillStyle: '#3F51B5',
+                                        lineWidth: 0}});
+
+    e1 = Vector.sub(sc.position, earth.position);
+    sc_positions = Array(128).fill([sc.position.x, sc.position.y]);
+    sc_positions_burn = Array(128).fill([sc.position.x, sc.position.y]);
+
+    Events.on(engine, 'afterUpdate', function(event) {
+        // fix earth
+        Body.setPosition(earth, center);
+
+        // rotate rocket
+        var l = sc_positions.length - 64;
+        sc_pos_old = Vector.create(sc_positions[l], sc_positions[l+1]);
+        var earth_sc_vec = Vector.sub(sc.position, earth.position);
+        var quadrant = 0;
+        if(earth_sc_vec.x > 0 && earth_sc_vec.y < 0) {
+            quadrant = Math.PI / 2;
+        } else if(earth_sc_vec.x > 0 && earth_sc_vec.y > 0) {
+            quadrant = -Math.PI / 2;
+        } else if(earth_sc_vec.x < 0 && earth_sc_vec.y > 0) {
+            quadrant = -Math.PI / 2;
+        } else if(earth_sc_vec.x < 0 && earth_sc_vec.y < 0) {
+            quadrant = Math.PI / 2;
+        }
+        var earth_sc_angle = quadrant - Math.atan(earth_sc_vec.x / earth_sc_vec.y);
+        Body.rotate(sc, earth_sc_angle - sc.angle);
+
+        // draw paths
+        sc_positions.push(sc.position.x);
+        sc_positions.push(sc.position.y);
+        if(fligh_path) {
+            draw_flight_path();
+            draw_flame();
+        }
+        if(burning) {
+            sc_positions_burn.push(sc.position.x);
+            sc_positions_burn.push(sc.position.y);
+//            if(fligh_path) {
+//                draw_flame();
+//            }
+        }
+        if(l > 2048+1024) {
+            sc_positions.shift();
+            sc_positions.shift();
+        }
+
+    });
+
+    // add all of the bodies to the world
+    World.add(world, earth);
+    World.add(world, sc);
+}
 
 function draw_flight_path() {
     context.beginPath();
@@ -82,60 +133,14 @@ function draw_flight_path() {
     context.closePath();
 }
 
-function draw_center_path() {
-    // draw path to center
+function draw_flame() {
     context.beginPath();
-    context.moveTo(earth.position.x, earth.position.y);
-    context.lineTo(sc.position.x, sc.position.y);
-    context.lineWidth = 2;
-    context.strokeStyle = '#F44336';
-    context.stroke();
-    context.closePath();
-    // draw tangent
-    context.beginPath();
-    context.moveTo(sc_pos_old.x, sc_pos_old.y);
-    context.lineTo(sc.position.x, sc.position.y);
-    context.lineWidth = 2;
-    context.strokeStyle = '#F44336';
+    context.curve(sc_positions_burn);
+    context.lineWidth = 4;
+    context.strokeStyle = '#F57F17';
     context.stroke();
     context.closePath();
 }
-
-var fligh_path = true;
-var center_path = false;
-sc_positions = Array(128).fill([sc.position.x, sc.position.y]);
-Events.on(engine, 'afterUpdate', function(event) {
-    // fix earth
-    Body.setPosition(earth, center);
-
-    // rotate rocket
-    var l = sc_positions.length - 64;
-    sc_pos_old = Vector.create(sc_positions[l], sc_positions[l+1]);
-    var earth_sc_vec = Vector.sub(sc.position, earth.position);
-    var quadrant = 0;
-    if(earth_sc_vec.x > 0 && earth_sc_vec.y < 0) {
-        quadrant = Math.PI / 2;
-    } else if(earth_sc_vec.x > 0 && earth_sc_vec.y > 0) {
-        quadrant = -Math.PI / 2;
-    } else if(earth_sc_vec.x < 0 && earth_sc_vec.y > 0) {
-        quadrant = -Math.PI / 2;
-    } else if(earth_sc_vec.x < 0 && earth_sc_vec.y < 0) {
-        quadrant = Math.PI / 2;
-    }
-    var earth_sc_angle = quadrant - Math.atan(earth_sc_vec.x / earth_sc_vec.y);
-    //Body.setAngle(sc, earth_sc_angle);
-    Body.rotate(sc, earth_sc_angle - sc.angle);
-
-    // draw paths
-    sc_positions.push(sc.position.x);
-    sc_positions.push(sc.position.y);
-    if(l > 2048) {
-        sc_positions.shift();
-        sc_positions.shift();
-    }
-    if(fligh_path) { draw_flight_path(); }
-    if(center_path) { draw_center_path(); }
-});
 
 function doSetTimeout(sc, force, start, i, STEP_t) {
     next_start = start + (i * STEP_t);
@@ -152,8 +157,10 @@ function burn(force, start, dt) {
     for(var i = 0; i < i_max; i++) {
         doSetTimeout(sc, force, start, i, STEP_t);
     }
+    setTimeout(function() {
+        burning = true;
+    }, start);
+    setTimeout(function() {
+        burning = false;
+    }, start + dt);
 }
-
-// add all of the bodies to the world
-World.add(world, earth);
-World.add(world, sc);
--- space.js
+++ space.js
@@ -1,27 +1,29 @@
-var runner = Runner.create();
+var runner = Runner.create({fps: 60});
 
-var first = true;
-var pause = true;
 fligh_path = true;
 center_path = false;
 
 function spacekey(key) {
     if (key == 13) { //enter
-        if(first) {
+        if(slide_no == 5) {
+            Runner.stop(runner);
+            create_space(slide_no);
+            burn({x: 0, y: -8.5*force}, 1000, 1000);
+        } else if(slide_no == 6) {
+            Runner.stop(runner);
+            create_space(slide_no);
+            burn({x: 0, y: -8.5*force}, 1000, 1000);
+            burn({x: 1.25*force, y: 0}, 2000, 5000);
+        } else if(slide_no == 7) {
+            Runner.stop(runner);
+            create_space(slide_no);
             burn({x: 0, y: -8.5*force}, 1000, 1000);
             burn({x: 2.5*force, y: 0}, 2000, 5000);
         }
         Runner.start(runner, engine);
-        first = false;
     } else if (key == 27) { // escape
         Runner.stop(runner);
-    } else if (key == 67) { // c
-        center_path = !center_path;
     } else if (key == 70) { // f
         fligh_path = !fligh_path;
     }
 }
-
-setTimeout(function() {
-        Runner.stop(runner);
-    }, 100000);
