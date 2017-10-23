cefa8cf46e0df3330529fc5d63a16fba514736a1
Sebastian Plamauer <oeplse@gmail.com>
2017-10-23 17:03:00 +0200

    Add solar system

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 var slide_no = 0;
+var first_2 = true;
+
+function do_js(slide_no) {
+    if(slide_no == 2 && first_2) {
+        window.open('http://mgvez.github.io/jsorrery/', '_blank');
+        first_2 = false;
+    }
+}
 
 window.onkeyup = function(e) {
     var key = e.keyCode ? e.keyCode : e.which;
@@ -15,13 +23,14 @@ window.onkeyup = function(e) {
     for(var i = 0; i < divsToHide.length; i++){
         divsToHide[i].style.display = "none";
     }
-    
+
     if(slide_no <= 0) {
         slide_no = 0;
     } else if (slide_no > divsToHide.length) {
         slide_no = divsToHide.length+1;
     } else {
         document.getElementById(slide_no.toString()).style.display = "block"
+        do_js(slide_no);
     }
     console.log(key, slide_no, divsToHide.length);
 };
